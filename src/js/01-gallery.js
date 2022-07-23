d4f1fedc1366e7d554ef66fb1fb57f171c3ac093
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-23 16:52:01 +0300

    task 2 change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkUp = galleryItems.map(({ original, preview, description }) => {
+    return `<a class="gallery__item" href="${original}"> <img class="gallery__image" src="${preview}" alt="${description}" /> </a>`
+}).join("");
+
+galleryRef.innerHTML = galleryMarkUp;
+
+
+galleryRef.addEventListener("click", event => {
+    event.preventDefault()
+});
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    
+    captionsData: "alt",
+    captionDelay: 250,
+ });
+
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+// import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+    // Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+
+
+player.on('timeupdate', function(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+    console.log(localStorage.getItem("videoplayer-current-time"));
+   
+});
+
+// Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
