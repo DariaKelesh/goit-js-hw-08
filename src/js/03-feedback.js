38a8e95faa151edbba3745d527543d2edd824b6d
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-24 09:57:55 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector("input[type=email]");
+const message = document.querySelector("textarea[name=message]");
+const LOCALSTORAGE_KEY = "feedback-form-state";
 
+updateLocalStorage();
+
+form.addEventListener("submit", setLocalStorage)
+
+function setLocalStorage(evt) {
+    evt.preventDefault();
+
+    localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
+    updateLocalStorage();
+    form.reset();
+}
+
+function updateLocalStorage() {
+    
+    localStorage.getItem(LOCALSTORAGE_KEY) || "";
+}
