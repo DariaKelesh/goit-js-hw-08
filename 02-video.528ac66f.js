ed4c3bbae24fd642092d85d36b0f71fafb6a4f57
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-07-25 14:41:42 +0000

    Deploying to gh-pages from @ DariaKelesh/goit-js-hw-08@7c2448e618c5def5ccbee3d043aa7a177c336ca6 🚀

--- 02-video.528ac66f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")})),t.getVideoTitle().then((function(e){console.log("title:",e)})),t.on("timeupdate",(function(e){localStorage.setItem("videoplayer-current-time",JSON.stringify(e)),console.log(localStorage.getItem("videoplayer-current-time"))})),t.setCurrentTime(30.456).then((function(e){})).catch((function(e){e.name}))}();
-//# sourceMappingURL=02-video.528ac66f.js.map
